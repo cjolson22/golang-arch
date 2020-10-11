4419682feecef1c536b7b4a0b2c651901b816a3b
Chris Olson <chrisolson@chriss-mbp.lan>
2020-10-11 17:58:34 -0500

    adding password hashing and decript

--- main.go
+++ main.go
@@ -1,10 +1,40 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
-	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
+	pass := "123456789"
+
+	hashedPass, err := hashPassword(pass)
+	if err != nil {
+		panic(err)
+	}
+
+	err = comparePassword(pass, hashedPass)
+	if err != nil {
+		log.Fatalln("Not logged in")
+	}
+	log.Println("Logged In")
+}
+
+func hashPassword(password string) ([]byte, error) {
+	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("Error whil generating bcrypt: %w", err)
+	}
+
+	return bs, nil
+}
+
+func comparePassword(password string, hashPass []byte) error {
+	err := bcrypt.CompareHashAndPassword(hashPass, []byte(password))
+	if err != nil {
+		return fmt.Errorf("Invalid password: %w", err)
+	}
+	return nil
 }
