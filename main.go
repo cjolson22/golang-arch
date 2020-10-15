0701f94952e5b549d69783f7421b43e41c2e549e
Chris Olson <chrisolson@chriss-mbp.lan>
2020-10-15 16:40:17 -0500

    updates for session

--- main.go
+++ main.go
@@ -7,9 +7,30 @@ import (
 
 func main() {
 	http.HandleFunc("/", foo)
+	http.HandleFunc("/", bar)
 	http.ListenAndServe(":8080", nil)
 }
 
+func getCode(msg string) string{
+	
+}
+func bar (w http.ResponseWriter, r *http.Request){
+	if r.Method != http.MethodPost{
+		http.Redirect(w,r, "/", http.StatusSeeOther)
+	}
+
+	email := r.FormValue("email")
+	if email == "" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+
+	c := http.Cookie{
+		Name: "session",
+		Value: email
+	}
+}
 func foo(w http.ResponseWriter, r *http.Request) {
 	html := `<!DOCTYPE html>
 	<html lang="en">
